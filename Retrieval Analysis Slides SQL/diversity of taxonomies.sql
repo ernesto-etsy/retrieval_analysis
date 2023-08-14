38a26fa91f6abb21c69e68be6637c717cfb94547
ernesto-etsy <128844223+ernesto-etsy@users.noreply.github.com>
2023-08-14 17:13:55 -0400

    Update diversity of taxonomies.sql

--- Retrieval Analysis Slides SQL/diversity of taxonomies.sql
+++ Retrieval Analysis Slides SQL/diversity of taxonomies.sql
@@ -2,22 +2,31 @@
 WITH CTE AS (
   SELECT DISTINCT visit_id, query, COUNT(DISTINCT taxonomy_id) AS variety_of_taxonomies
   FROM `etsy-data-warehouse-prod.rollups.organic_impressions`
-  WHERE _date >= CURRENT_DATE() - 5
+  WHERE _date >= CURRENT_DATE() - 90
   AND placement = 'search'
+  AND query <> ""
   GROUP BY 1,2
-)
+),
+CTE2 AS (
 SELECT
-top_category,
-AVG(CASE WHEN label = 'Broad' THEN 1 ELSE 0 END) AS pct_broad_queries,
-AVG(b.variety_of_taxonomies) AS avg_taxonomies_in_results,
+query_session_id,
+label,
+variety_of_taxonomies,
+same_day_gms
 FROM `etsy-data-warehouse-dev.ecanales.retrieval_search_data_one_year` a
 JOIN CTE b
   ON a.visit_id = b.visit_id
   AND a.query_raw = b.query
 WHERE a.total_listings IS NOT NULL
---AND label IS NOT NULL
-AND top_category IS NOT NULL
-GROUP BY 1;
+)
+SELECT
+label,
+variety_of_taxonomies,
+SUM(same_day_gms) / COUNT(DISTINCT query_session_id) AS gms_per_query,
+COUNT(DISTINCT query_session_id) AS total_queries
+FROM CTE2
+GROUP BY 1,2
+ORDER BY 2;
 
 --Diversity of Taxonomies
 WITH CTE AS (
@@ -25,16 +34,27 @@ WITH CTE AS (
   FROM `etsy-data-warehouse-prod.rollups.organic_impressions`
   WHERE _date >= CURRENT_DATE() - 90
   AND placement = 'search'
+  AND query <> ""
   GROUP BY 1,2
-)
+),
+CTE2 AS (
 SELECT
+query_session_id,
 label,
-AVG(b.variety_of_taxonomies) AS avg_taxonomies_in_results,
+variety_of_taxonomies,
+same_day_gms
 FROM `etsy-data-warehouse-dev.ecanales.retrieval_search_data_one_year` a
 JOIN CTE b
   ON a.visit_id = b.visit_id
   AND a.query_raw = b.query
 WHERE a.total_listings IS NOT NULL
---AND label IS NOT NULL
-AND top_category IS NOT NULL
-GROUP BY 1;
+AND top_category = "home_and_living"
+)
+SELECT
+label,
+variety_of_taxonomies,
+SUM(same_day_gms) / COUNT(DISTINCT query_session_id) AS gms_per_query,
+COUNT(DISTINCT query_session_id) AS total_queries
+FROM CTE2
+GROUP BY 1,2
+ORDER BY 2;
